652acc04dde5054b280f5eed425862313bc604ea
TeraBasedProgrammer <ilyadronov2332@gmail.com>
2022-05-17 00:43:53 +0300

    Implemented a filter button and a custom scrollbar

---
+++ DetailsHandbook/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using DetailsHandbook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DetailsHandbook
+{
+    public class CustomButton : Button
+    {
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CustomButton), new PropertyMetadata());
+    }
+}
--- DetailsHandbook/MainWindow.xaml.cs
+++ DetailsHandbook/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -17,13 +16,53 @@ namespace DetailsHandbook
 {
     public partial class MainWindow : Window
     {
-        private Dictionary<Button, Detail> buttonObjectPairs = new();
+        private Dictionary<CustomButton, Detail> buttonObjectPairs = new();
+        private Dictionary<CustomButton, WrapPanel> filterPanelPairs = new();
+        
         public MainWindow()
         {
             InitializeComponent();
+            AddFilters(); 
             Render();
         }
-       
+
+        private void AddFilters()
+        {
+            WrapPanel plug = new WrapPanel();
+            filterPanelPairs.Add(NoneFilterCheckBox, plug);
+            filterPanelPairs.Add(CapasitorsCheckBox, capasitorsWrapPannel);
+            filterPanelPairs.Add(CommProductsCheckBox, commProductsWrapPannel);
+            filterPanelPairs.Add(DiodesCheckBox, diodesWrapPannel);
+            filterPanelPairs.Add(MicrocircuitsCheckBox, microcircuitsWrapPannel);
+            filterPanelPairs.Add(ResistorsCheckBox, resistorsWrapPannel);
+            filterPanelPairs.Add(InductancesCheckBox, inductancesWrapPannel);
+            filterPanelPairs.Add(ThyristorsCheckBox, thyristorsWrapPannel);
+            filterPanelPairs.Add(TransistorsCheckBox, transistorsWrapPannel);
+            filterPanelPairs.Add(ZenerDiodesCheckBox, zenerDiodesWrapPannel);
+        }
+
+        private void FilterOut(CustomButton value)
+        {
+            foreach (WrapPanel wp in filterPanelPairs.Values)
+            {
+                wp.Visibility = Visibility.Collapsed;
+            }
+            filterPanelPairs.GetValueOrDefault(value).Visibility = Visibility.Visible;
+        }
+        
+        private void ClearFilterPoint()
+        {
+            foreach (CustomButton button in filterPanelPairs.Keys)
+            {
+                TextBlock temp = (TextBlock)button.Content;
+                if (temp.Text.Contains("• "))
+                {
+                    temp.Text = temp.Text.Substring(2);
+                    button.Content = temp;
+                    break;
+                }
+            }
+        }
         private void Render()
         {
             using(var db = new DetailsDbContext())
@@ -56,7 +95,7 @@ namespace DetailsHandbook
             buttonContent.Children.Add(buttonTypeText);
             buttonContent.Children.Add(buttonModelText);
 
-            var b = new Button();
+            var b = new CustomButton();
             b.Style = (Style)Resources["DetailButtonStyle"];
 
             b.Click += DetailButtonClick;
@@ -65,9 +104,23 @@ namespace DetailsHandbook
             wp.Children.Add(b);
         }
 
+        private void WindowPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void WindowCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WindowHideButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
         private void DetailButtonClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(buttonObjectPairs.GetValueOrDefault((Button)sender).ToString());
+            MessageBox.Show(buttonObjectPairs.GetValueOrDefault((CustomButton)sender).ToString());
         }
 
         private void FilterButton_Click(object sender, RoutedEventArgs e)
@@ -76,6 +129,27 @@ namespace DetailsHandbook
                 FiltersBar.Visibility = Visibility.Visible;
             else
                 FiltersBar.Visibility = Visibility.Hidden;
+        }
+
+        private void NoneFilterChexBox_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(WrapPanel wp in filterPanelPairs.Values)
+            {
+                wp.Visibility = Visibility.Visible;
+            }
+            ClearFilterPoint();
+            TextBlock tempTextBlock = (TextBlock)NoneFilterCheckBox.Content;
+            tempTextBlock.Text = "• " + tempTextBlock.Text;
+            NoneFilterCheckBox.Content = tempTextBlock;
+        }
+        private void FilterCheckBoxes_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFilterPoint();
+            CustomButton tempButton = (CustomButton)sender;
+            TextBlock tempTextBlock = (TextBlock)tempButton.Content;
+            tempTextBlock.Text = "• " + tempTextBlock.Text;
+            sender = tempButton;
+            FilterOut((CustomButton)sender);
         }
     }
 }
